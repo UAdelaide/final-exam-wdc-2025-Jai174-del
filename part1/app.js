7fa37b42ea7451e38b68ae49af9d8f7fe4cd6a37
krishya <krishyamakkar37@gmail.com>
2025-06-21 06:06:27 +0000

    [automatic commit] 2025-06-21 06:06:26 UTC 118.211.67.231

--- part1/app.js
+++ part1/app.js
@@ -6,22 +6,22 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var searcheRouter = require('./routes/searches');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/api', searcheRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -39,3 +39,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
