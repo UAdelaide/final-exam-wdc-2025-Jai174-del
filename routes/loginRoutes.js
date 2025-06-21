6a9a46129e5e7cd5408d96b618e3172ca04fbbb5
krishya <krishyamakkar37@gmail.com>
2025-06-21 04:09:54 +0000

    [automatic commit] 2025-06-21 04:09:53 UTC 118.211.67.231

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); // Adjust path if your DB connection file is named differently
+const db = require('../db');
 
 router.post('/', async (req, res) => {
   const { username, password } = req.body;
