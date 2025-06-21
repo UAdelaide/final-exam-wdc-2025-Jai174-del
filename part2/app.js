284d418e6804a0e15ae5dd5f1c5478b66b804047
krishya <krishyamakkar37@gmail.com>
2025-06-21 06:16:23 +0000

    [automatic commit] 2025-06-21 06:16:22 UTC 118.211.67.231

---
+++ part2/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(session({
+    secret: 'mydogwalkerapp@123!#$buddy',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
+const searchesRoutes = require('./routes/searchesRoutes');
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/dogs', searchesRoutes);
+
+// Export the app instead of listening here
+module.exports = app;
