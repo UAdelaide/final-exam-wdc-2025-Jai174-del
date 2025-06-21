9d37684a1a702c4a54047126692c241b36ccedac
krishya <krishyamakkar37@gmail.com>
2025-06-21 06:19:46 +0000

    [automatic commit] 2025-06-21 06:19:45 UTC 118.211.67.231

--- part2/routes/searchesRoutes.js
+++ part2/routes/searchesRoutes.js
@@ -1,18 +1,2 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../models/db');
 
-// GET /api/dogs - fetch all dogs
-router.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.query(
-      SELECT d.dog_id, d.name, d.size, d.owner_id
-      FROM Dogs d
-    );
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch dogs.' });
-  }
-});
 
-module.exports = router;
