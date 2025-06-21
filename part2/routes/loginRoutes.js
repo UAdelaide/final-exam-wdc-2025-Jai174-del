ebc8d14539fb8008146bdc1dec1f4b476f70858a
krishya <krishyamakkar37@gmail.com>
2025-06-21 04:56:25 +0000

    [automatic commit] 2025-06-21 04:56:24 UTC 118.211.67.231

--- part2/routes/loginRoutes.js
+++ part2/routes/loginRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../models/db');
+
 
 router.post('/', async (req, res) => {
   const { username, password } = req.body;
